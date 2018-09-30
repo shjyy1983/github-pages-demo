fd75ac1a312298ea7df947afb79a55bf481b169c
SHEN <>
2018-09-30 15:08:50 +0800

    dev

---
+++ config.js
@@ -0,0 +1,13 @@
+/**
+ *
+ * @authors liwb (you@example.org)
+ * @date    2017/2/3 13:57
+ * @version $ 根据不同环境配置对应的服务地址及变量，这里只放置必须要手动修改的变量
+ */
+
+/* name module */
+
+window.LOCAL_CONFIG = {
+  // API_HOME: 'http://127.0.0.1:3001/api',
+  API_HOME: 'http://112.124.103.171:3001/api'
+};
